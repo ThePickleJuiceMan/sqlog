643e8c06558f90886640188f013f97de709dcc04
jeff <jeffrey.martin12519@gmail.com>
2022-04-04 14:59:34 -0500

    added internal logging

--- sqlog.c
+++ sqlog.c
@@ -1,18 +1,57 @@
 #include "sqlog.h"
 
 int execute_log_ddl(sqlog *);
-int execute_log_dml(sqlog *, char * log_text);
+int execute_internal_log_ddl(sqlog *);
+int execute_write_log_dml(sqlog *, char * log_text);
+int execute_read_log_dml(sqlog *, int entries);
 
-int create_log(char * log_db_name, sqlog ** log, int create_new)
+int create_log(char * log_db_name, sqlog ** log, int create_new, int internal_logging)
 {
 	(*log) = (sqlog *)malloc(sizeof(sqlog));
 	(*log)->dbname = log_db_name;
+	(*log)->internal_log_enabled = internal_logging;
+	
+	int retval;
+	if(internal_logging)
+	{
+		//set up internal log at log_db_name directory
+
+		char *log_dir = strdup(log_db_name);
+		log_dir = dirname(log_dir);
+		char internal_log_name[255];
+
+		strcat(internal_log_name, log_dir);
+		strcat(internal_log_name, "/internal_sqlog.db");
+
+		(*log)->internal_dbname = internal_log_name;
+
+		if(sqlite3_open_v2((*log)->internal_dbname, &(*log)->internal_sql, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL) != SQLITE_OK)
+		{
+			fprintf(stderr, "Unable to start internal sqlog logging\n");
+			(*log)->internal_log_enabled = 0;
+		}
+
+		if(sqlite3_table_column_metadata((*log)->internal_sql, "", "internal_sqlog", NULL,NULL,NULL,NULL,NULL,NULL) != SQLITE_OK)
+		{
+			if(execute_internal_log_ddl((*log)) != SQLITE_OK)
+			{
+				fprintf(stderr, "Call to execute_internal_log_ddl failed. No internal logging will take place.\n");
+				(*log)->internal_log_enabled = 0;
+			}
+		}
+
+		(*log)->internal_log_enabled = 1;
+	}
 
 	if(create_new)
 	{
 		int sql_open = sqlite3_open_v2((*log)->dbname, &(*log)->sql, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
 		if(sql_open != SQLITE_OK)
 		{
+			if((*log)->internal_log_enabled)
+			{
+
+			}
 			fprintf(stderr, "sqlite_open_v2 return %d\n", sql_open);
 			return -1;
 		}
@@ -22,14 +61,16 @@ int create_log(char * log_db_name, sqlog ** log, int create_new)
 		{
 			//log table does not exist, this is probably a new log file. create it
 
-			return execute_log_ddl((*log));
+			retval = execute_log_ddl((*log));
+			fprintf(stdout, "Log created: %d\n", retval);
 		}
 
 	}
 	else
 	{
-		return sqlite3_open((*log)->dbname, &(*log)->sql);
+		retval = sqlite3_open((*log)->dbname, &(*log)->sql);
 	}
+
 }
 
 //TODO: open log is probably not needed with create_new parameter on create_log
@@ -40,14 +81,20 @@ int open_log(sqlog *log, char * database)
 	return sql_open;
 }
 
-int read_log(sqlog *sql, int entries)
+int read_log(sqlog *log, int entries)
 {
-	return 0;
+	return execute_read_log_dml(log, entries);
 }
 
 int write_log(sqlog *log, char * log_text)
 {
-	return execute_log_dml(log, log_text);
+	return execute_write_log_dml(log, log_text);
+}
+
+int write_log_internal(sqlog * log, char * log_text, int log_type)
+{
+
+
 }
 
 int clear_log(sqlog *log, int archive)
@@ -89,7 +136,14 @@ int execute_log_ddl(sqlog * log)
 	return sqlite3_exec(log->sql, log_ddl, NULL,NULL,NULL);
 }
 
-int execute_log_dml(sqlog *log, char * log_text)
+int execute_internal_log_ddl(sqlog * log)
+{
+	const char * log_ddl = "CREATE TABLE \"internal_sqlog\" (\"logkey\" INTEGER NOT NULL UNIQUE, \"logtype\" TEXT NOT NULL, \"logtime\" INTEGER NOT NULL,\"logtext\" TEXT NOT NULL,PRIMARY KEY(\"logkey\"));";
+	return sqlite3_exec(log->internal_sql, log_ddl, NULL,NULL,NULL);
+}
+
+
+int execute_write_log_dml(sqlog *log, char * log_text)
 {
 	//check if statement exists on log struct, if not prepare statement
 	if(log->lstmt != NULL)
@@ -142,4 +196,9 @@ int execute_log_dml(sqlog *log, char * log_text)
 	sqlite3_exec(log->sql, "COMMIT;", NULL, NULL, NULL);
 	return SQLITE_OK;
 
+}
+
+int execute_read_log_dml(sqlog * log, int entries)
+{
+	return 0;
 }
--- sqlog.h
+++ sqlog.h
@@ -7,6 +7,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <time.h>
+#include <libgen.h>
 #endif
 
 typedef struct sqlog
@@ -17,10 +18,21 @@ typedef struct sqlog
 	sqlite3_stmt * lstmt; //this will be used to continuously execute logging without re-preparing sql each time.
 
 	//int fifo; currently unused named pipe file descriptor
+	
+	//internal logging for sqlog issues
+	int internal_log_enabled;
+	sqlite3 * internal_sql;
+	char * internal_dbname;
 } sqlog;
 
+enum logtype 
+{
+	SQLOG_INFO = 0,
+	SQLOG_ERROR = 1
+};
+
 //Public functions for sqlog.c
-int create_log(char *, sqlog **, int);
+int create_log(char *, sqlog **, int, int);
 int open_log(sqlog *, char *);
 int read_log(sqlog *, int);
 int write_log(sqlog *, char *);
